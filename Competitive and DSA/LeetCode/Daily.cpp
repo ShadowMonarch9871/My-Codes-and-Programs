ec198b8cc99a2838178591540765694bb15f73ca
Ayush Pokhriyal <ayushpokhriyal21@gmail.com>
2024-05-23 21:49:29 +0530

    LeetCode daily problem

--- Competitive and DSA/LeetCode/Daily.cpp
+++ Competitive and DSA/LeetCode/Daily.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 class Solution {
 public:
+	vector<vector<int>>arrays;
 	vector<vector<int>> subsets(vector<int>& nums) {
 		vector<vector<int>> ans;
 		ans.push_back(vector<int>());
@@ -24,6 +25,26 @@ public:
 		}
 		return ans;
 	}
+	int beautifulSubsets(vector<int>& nums, int k) {
+		int count = 0;
+		int i = 0;
+		while (i < nums.size()) {
+			int j = i;
+			while (j < nums.size()) {
+				if (abs(nums[i] - nums[j]) != k) {
+					count++;
+					vector<int>temp;
+					temp.push_back(nums[i]);
+					temp.push_back(nums[j]);
+					arrays.push_back(temp);
+				}
+				j++;
+			}
+			i++;
+		}
+		return count;
+
+	}
 };
 int main() {
 	vector<int>v;
@@ -31,13 +52,16 @@ int main() {
 	while (cin >> input) {
 		v.push_back(input);
 	}
+	int k = 1;
+	// cin >> k;
 	Solution sol;
-	vector<vector<int>>ans=sol.subsets(v);
-	for(auto it:ans){
-		for(auto it1:it){
-			cout<<it1;
+	int ans = sol.beautifulSubsets(v, k);
+	cout << ans << endl;
+	for (auto it : sol.arrays) {
+		for (auto it1 : it) {
+			cout << it1;
 		}
-		cout<<endl;
+		cout << endl;
 	}
 
 }
